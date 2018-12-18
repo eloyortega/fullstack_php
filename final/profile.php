a0be9ff883d9a0ba77d2b84e96db6341548575b5
eloy ortega <info@artbyeloi.com>
2018-12-18 13:05:00 -0500

    added comments

--- final/profile.php
+++ final/profile.php
@@ -15,10 +15,14 @@
 		</tr>
 
 	<?php
+		//while in the table, get the json data
 		$d = file_get_contents('assets/data.json');
+		//convert the data to a php array so we can work with it
 		$d = json_decode($d, true);
 		
+		//loop through the data to create dynamic html
 		foreach($d as $k => $o){
+			//for each object in our data, create a table row with table cells. in each table cell, concatenate values from the object we are looping through
 			echo '<tr>
 					<td>'.++$k.'</td>
 					<td>'.$o['name'].'</td>
